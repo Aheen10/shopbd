d029906226aa8404fec1821ab0fea672ba93816c
Aheen Khan <khan2305341410@diu.edu.bd>
2026-06-20 14:24:03 +0600

    Add: migration - add_otp

---
+++ backend/prisma/migrations/20260617045358_add_otp/migration.sql
@@ -0,0 +1,15 @@
+-- AlterTable
+ALTER TABLE `product` MODIFY `emoji` VARCHAR(191) NOT NULL DEFAULT '📦';
+
+-- CreateTable
+CREATE TABLE `OTP` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `target` VARCHAR(191) NOT NULL,
+    `code` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `expiresAt` DATETIME(3) NOT NULL,
+    `used` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
